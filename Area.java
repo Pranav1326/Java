a07369f48b6f06f639ebcb7fa12dc8717b8bf7ba
Pranav <63364919+Pranav1326@users.noreply.github.com>
2022-02-24 19:41:30 +0530

    Practical Program-2 upload

---
+++ Area.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Shape{
+	public int area(int width, int height){
+		return (width * height);
+	}
+	public float area(int rad){
+		final float pi = 3.14;
+		float a = (pi * rad * rad);
+		return a;
+	}
+	public int area(int base, int height){
+		return (base * height);
+	}
+}
+
+class Area{
+	public static void main(String[] args){
+		
+	}
+}
